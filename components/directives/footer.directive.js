9585ce023e66b4a60e30ed170d0bfc6a73a046ac
tanmoythander <tanmoythander@gmail.com>
2019-05-15 00:43:29 +0600

    Gulp@4.0.2

--- components/directives/footer.directive.js
+++ components/directives/footer.directive.js
@@ -10,7 +10,7 @@
 		// Definition of directive
 		var directiveDefinitionObject = {
 			restrict: 'E',
-			templateUrl: 'components/directives/footer.html'
+			templateUrl: '/components/directives/footer.html'
 		};
 
 		return directiveDefinitionObject;
